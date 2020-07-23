a1cf144c60b7687ebe5548c3b52b7ef91eb4e0ae
Karim Alomari <karim.alomari@gmail.com>
2020-07-23 19:50:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  numbers = [first, second, third, fourth]
+  numbers = [knicks, nets, celtics, wizards]
 end
 
 def add_element_to_end_of_array(array, element)
