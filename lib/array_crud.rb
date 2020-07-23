d55ff21842661f65971be69f44ed8d37c277674e
Karim Alomari <karim.alomari@gmail.com>
2020-07-23 19:54:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  nba_teams = ["Knicks", "Nets", "Celtics", "Wizards"]
+  array = ["wow", "I", "am", "really", "learning"]
   nba_teams << "arrays"
 end
 
